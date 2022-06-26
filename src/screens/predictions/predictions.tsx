625d91c688c498b3635ea1d274a29fbb12b2104b
hyacinthe <hyacinthe@codehesion.co.za>
2022-06-26 12:24:39 +0200

    new option added

--- src/screens/predictions/predictions.tsx
+++ src/screens/predictions/predictions.tsx
@@ -25,7 +25,7 @@ const PredictionsScreen: React.FC = () => {
   })
   const betOptions: {name: String; id: Number} [] = [{name: 'Both Teams to Score', id: 0}, {name: 'Home/Away', id: 1}, {name: 'Home/Away over 1.5', id: 2},
   {name: 'over 1.5', id: 3}, {name: 'Over 2.5', id: 4}, {name: 'Home/Away Wins Either Half', id: 5 }, {name: 'Multi Goals (2-5) Goals', id: 6}, {name: 'Multi Goals (3-6) Goals', id: 7},
-  {name: 'Both Halfs Over 0.5', id: 8}, {name: 'Draw or GG', id: 9 }, {name: 'Draw', id: 10}
+  {name: 'Both Halfs Over 0.5', id: 8}, {name: 'Draw or GG', id: 9 }, {name: 'Draw', id: 10}, {name: 'Half-Time Draw', id: 11}
 ]
   
   useEffect(()=>{
