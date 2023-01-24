5706cf05fd0540bae0c00870fc9a388be0ad62ee
hyacinthe <hyacinthe.ebula21@gmail.com>
2023-01-24 17:56:24 +0200

    small change

--- src/prediction-functions/home-clean-sheet.ts
+++ src/prediction-functions/home-clean-sheet.ts
@@ -39,7 +39,7 @@ export const predictHomeCleanSheet = ({
       }
       //TODO filter the fixtures that passes the H wins either half test here and return it
       return (
-        againstHomeTeamGoalsPercentage({ homeTeamStanding }) <= 85 &&
+        againstHomeTeamGoalsPercentage({ homeTeamStanding }) <= 90 &&
         awayTeamGoalsPercentage({ awayTeamStanding }) <= 90  && awayTeamFailScroringInMostAwayFixtures({awayfixtures: lastFiveAwayTeamAwayFixtures})
       );
     });
