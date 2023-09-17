25379eade364151edb1302877d532929672f2ddd
hyacinthe <hyacinthe.ebula21@gmail.com>
2023-09-17 13:44:48 +0200

    more changes

--- src/prediction-functions/over_0_5.ts
+++ src/prediction-functions/over_0_5.ts
@@ -46,7 +46,7 @@ export const predictOver0_5 = ({
       ) {
         return false;
       }
-      return (homeTeamGoalsPercentage({homeTeamStanding})>= 110 && awayTeamGoalsPercentage({awayTeamStanding})>=110) && fixtureH2hFixtures.every(fixture=> fixture.goals.away + fixture.goals.home >= 2);
+      return (homeTeamGoalsPercentage({homeTeamStanding})>= 120 && awayTeamGoalsPercentage({awayTeamStanding})>=120) && fixtureH2hFixtures.every(fixture=> fixture.goals.away + fixture.goals.home >= 1);
     });
     return {
       fixtures: predictedFixtures,
