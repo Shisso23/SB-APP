10b422dc3b6cb356fbc504195096b674ae1ddfde
hyacinthe <hyacinthe.ebula21@gmail.com>
2023-10-21 12:54:35 +0200

    btts modified

--- src/prediction-functions/both-tems-score.ts
+++ src/prediction-functions/both-tems-score.ts
@@ -64,9 +64,9 @@ export const predictBothTeamsToScore = ({
       // ) || (homeTeamScroreInMostH2HFixtures({h2hFixtures, homeTeamId,minGoals: 1}) && awayTeamScroreInMostH2HFixtures({awayTeamId,h2hFixtures,minGoals: 1}) &&  homeTeamGoalsPercentage({homeTeamStanding})>=150 && awayTeamGoalsPercentage({awayTeamStanding})>=150)))
       // || (homeTeamGoalsPercentage({homeTeamStanding})>= 180 && awayTeamGoalsPercentage({awayTeamStanding})>=180 && againstAwayTeamGoalsPercentage({awayTeamStanding}) >=130 && againstHomeTeamGoalsPercentage({homeTeamStanding}) >=130))
       //  && hasNoNilNilInFixtures({fixtures: h2hFixtures})  && hasNoNilNilInFixtures({fixtures: lastFiveHomeTeamHomeFixtures})  && hasNoNilNilInFixtures({fixtures: lastFiveAwayTeamAwayFixtures}); 
-      if(lastFiveAwayTeamAwayFixtures.length<3 || lastFiveHomeTeamHomeFixtures.length<3 || homeTeamStanding.all.played<3 || awayTeamStanding.all.played< 3) return false
-      return ( (awayTeamStanding.rank <5 && Math.abs(awayTeamStanding.rank - homeTeamStanding.rank)>=5 && awayTeamWinsMostMatchesTimes({fixtures: lastFiveAwayTeamAwayFixtures, awayTeamId: lastFiveAwayTeamAwayFixtures[0].teams.away.id})) && 
-      (awayTeamStanding.points - homeTeamStanding.points)>6 && homeTeamGoalsPercentage({homeTeamStanding})>=150)
+      if(!homeTeamStanding || !awayTeamStanding ||lastFiveAwayTeamAwayFixtures.length < 3 ||  awayTeamStanding.all.played<3) return false
+      return ( (awayTeamStanding.rank <5 && Math.abs(awayTeamStanding.rank - homeTeamStanding.rank)>=4 && awayTeamWinsMostMatchesTimes({fixtures: lastFiveAwayTeamAwayFixtures, awayTeamId: lastFiveAwayTeamAwayFixtures[0].teams.away.id})) && 
+      (awayTeamStanding.points - homeTeamStanding.points)>5 && homeTeamGoalsPercentage({homeTeamStanding})>=150)
 
     });
     return {
