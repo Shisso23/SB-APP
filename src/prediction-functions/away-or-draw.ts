1458039814f7f220ebf5fd60c4de2474c1c63087
hyacinthe <hyacinthe.ebula21@gmail.com>
2023-12-31 00:57:46 +0200

    small change

--- src/prediction-functions/away-or-draw.ts
+++ src/prediction-functions/away-or-draw.ts
@@ -37,7 +37,9 @@ export const predictAwayOrDraw = ({
         return false;
       }
       //TODO filter the fixtures that passes the H wins either half test here and return it
-      return goodAwayTeamwinPercentage({awayStanding: awayTeamStanding, homeStanding: homeTeamStanding, lossPercentage: 70, winPercentage: 45})// away team wins atleast  winPercentage of their games and hometeam loses at least lossPercentage of games
+      return  ((awayTeamStanding.rank <=4 && Math.abs(homeTeamStanding.rank - awayTeamStanding.rank)> 5 && awayTeamWinsMostMatchesTimes({fixtures: lastFiveAwayTeamAwayFixtures, awayTeamId: lastFiveAwayTeamAwayFixtures[0].teams.away.id}) )&& 
+      (awayTeamStanding.points - homeTeamStanding.points)>5 && 
+     ( homeTeamFailScroringInMostHomeFixtures({homefixtures: lastFiveHomeTeamHomeFixtures})) ) || goodAwayTeamwinPercentage({awayStanding: awayTeamStanding, homeStanding: homeTeamStanding, lossPercentage: 50, winPercentage: 60})
     });
     return {
       fixtures: predictedFixtures,
