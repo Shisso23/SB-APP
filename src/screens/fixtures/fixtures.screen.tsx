e65f559d91e456e16b78b7537e7967ba052c02a3
hyacinthe <hyacinthe.ebula21@gmail.com>
2024-03-23 12:56:30 +0200

    added count inside same div as predictions title text

--- src/screens/fixtures/fixtures.screen.tsx
+++ src/screens/fixtures/fixtures.screen.tsx
@@ -476,10 +476,8 @@ const FixturesScreen: React.FC = () => {
             className=" flex font-bold self-center text-lg py-2 items-center justify-center text-center text-m border rounded-lg bg-yellow-500 w-40 
         sm:w-64 sm:mb-5 mb-2 "
           >
-            Predictions
+            Predictions: {groupedData &&Object.keys(groupedData).length}
           </div>
-          <div className=" flex font-bold self-center text-lg py-2 items-center justify-center text-center text-m border rounded-lg bg-yellow-500 w-20 
-        sm:w-64 sm:mb-5 mb-2  ">{groupedData &&Object.keys(groupedData).length}</div>
           <>
             {loadingLeaguesFixtures ? (
               <CircularProgress />
