2fc8fde84f4309cd1179f00da35398ef212063d5
Hyacinthe <hyacinthe.shisso@investec.com>
2024-11-04 17:13:34 +0200

    added all kind of leagues

--- src/screens/bet/bet.tsx
+++ src/screens/bet/bet.tsx
@@ -27,7 +27,6 @@ const BetScreen: React.FC = () => {
   const [windowHeight, setWindowHeight] = useState(window.innerHeight)
   const [leaguesFilters, setLeaguesFilters] = useState<LeaguesFilterModel>({
     current: true,
-    type: 'league',
   })
   const dispatch: any = useDispatch()
   const [searchedLeagues, setSearchedLeagues] = useState<
