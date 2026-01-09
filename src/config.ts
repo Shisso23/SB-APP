e6c4ef3221ced8384befd685cd9201824cf0d239
hyacinthe <hyacinthe.shisso@investec.com>
2026-01-09 09:20:12 +0200

    Changed base url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export default   {
     apiKey: process.env.REACT_APP_API_KEY,
     xApiSportsKey: process.env.X_API_SPORTS_KEY,
-    apiUrl: 'https://api-football-v1.p.rapidapi.com/v3/'
+    // apiUrl: 'https://api-football-v1.p.rapidapi.com/v3/'
+    apiUrl: 'https://v3.football.api-sports.io/'
 }
