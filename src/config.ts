b245ba9eb958ce469ea0de65a5d9c7b201ceed28
hyacinthe <hyacinthe.shisso@investec.com>
2026-01-09 09:37:07 +0200

    Hardcoded key

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export default   {
     apiKey: process.env.REACT_APP_API_KEY,
-    xApiSportsKey: process.env.X_API_SPORTS_KEY,
+    // xApiSportsKey: process.env.X_API_SPORTS_KEY,
+    xApiSportsKey:'10a431e50d961756f81938b1c9733eae',
     // apiUrl: 'https://api-football-v1.p.rapidapi.com/v3/'
     apiUrl: 'https://v3.football.api-sports.io/'
 }
